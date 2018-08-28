fda4ebc5767cf1bd24e266f1ef8cab1272a92e22
David Hansen <david.hansen.0000@gmail.com>
2018-08-28 20:53:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a+b
+}
+
+function subtrace(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
+
+function makeInt(n) {
+  if (n instanceof String.class) {
+    return throw 
+  } else {
+    return parseInt(n, 10)
+  }
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
